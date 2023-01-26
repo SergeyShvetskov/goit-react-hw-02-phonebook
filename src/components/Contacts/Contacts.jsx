6d7b2e15ec53c2110c5183126df03aba9ed2a1cd
User61 <svetsoff@gmail.com>
2023-01-26 20:54:43 +0200

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -7,52 +7,55 @@ import Notification from '../Notification';
 class Contacts extends Component {
   state = {
     filter: '',
-  }
+  };
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     this.setState({ filter: e.target.value });
   };
 
   render() {
-    const {filter} = this.state;
+    const { filter } = this.state;
     const { contacts, onClickDelete } = this.props;
-    const filteredContacts = contacts.filter(it => it.name.includes(filter))
+    const filteredContacts = contacts.filter(it => it.name.includes(filter));
 
     return (
       <div className={s.container}>
         <InputField
-          label='Find contacts by name'
+          label="Find contacts by name"
           value={filter}
           onChange={this.handleInputChange}
-          type='text'
-          name='filter'
+          type="text"
+          name="filter"
         />
 
-        {!filteredContacts.length
-          ? <Notification message='Contact list is empty.' />
-          : <ul className={s.list}>
+        {!filteredContacts.length ? (
+          <Notification message="Contact list is empty." />
+        ) : (
+          <ul className={s.list}>
             {filteredContacts.map(({ id, name, number }) => (
               <li key={id} className={s.item}>
                 <span className={s.name}>{name}</span>
                 <span>{number}</span>
-                <button
-                  type='button'
-                  onClick={() => onClickDelete(id)}
-                >Delete</button>
+                <button type="button" onClick={() => onClickDelete(id)}>
+                  Delete
+                </button>
               </li>
             ))}
-          </ul>}
+          </ul>
+        )}
       </div>
     );
   }
 }
 
 Contacts.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  })).isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   onClickDelete: PropTypes.func.isRequired,
 };
 
