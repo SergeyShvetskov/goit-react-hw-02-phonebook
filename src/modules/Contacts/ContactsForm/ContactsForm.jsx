eec882b21a67adb361e3bba23ec16fa21405432a
User61 <svetsoff@gmail.com>
2023-02-04 18:16:28 +0200

    Update ContactsForm.jsx

--- src/modules/Contacts/ContactsForm/ContactsForm.jsx
+++ src/modules/Contacts/ContactsForm/ContactsForm.jsx
@@ -40,7 +40,10 @@ class ContactForm extends Component {
             name="name"
             value={name}
             onChange={handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             placeholder=""
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
           />
         </div>
         <div className={styles.formGroup}>
@@ -49,7 +52,10 @@ class ContactForm extends Component {
             name="number"
             value={number}
             onChange={handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             placeholder=""
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
           />
         </div>
         <button className={styles.btn} type="submit">
